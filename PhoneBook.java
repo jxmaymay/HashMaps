e7eab270ad678d6cc793991df0ae50b6b5a49ac5
jxmaymay <31222311+jxmaymay@users.noreply.github.com>
2018-03-06 10:29:10 -1000

    Add files via upload

--- PhoneBook.java
+++ PhoneBook.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class PhoneBook {
 	public static void main(String[] args) {
-		Map<String, Integer> phoneBook = new HashMap<String, Integer>();
+		Map<String, Double> phoneBook = new HashMap<String, Double>();
 		
 		boolean run = true;
 		
@@ -18,10 +18,10 @@ public class PhoneBook {
 				//int phoneNum;
 				
 				for (int i = 0; i < newEntries; i++) {
-					System.out.println("Name of entry # " + i + ": ");
+					System.out.println("Name of entry # " + (i + 1) + ": ");
 					String name = scnr.next();
-					System.out.println("Phone number of entry # " + i + ": ");
-					int phoneNum = scnr.nextInt(s)
+					System.out.println("Phone number of entry # " + (i + 1) + ": ");
+					double phoneNum = scnr.nextDouble();
 					
 					phoneBook.put(name, phoneNum);
 					
@@ -29,7 +29,15 @@ public class PhoneBook {
 				
 				System.out.println("All entires added.");
 			} else if (option.equals("search")) {
-				
+				System.out.println("Who's number would you like to search for?");
+				String searchName = scnr.next();
+				double foundNum;
+				boolean found = false;
+				phoneBook.forEach((String, Double) -> if (searchName.equals(String) {
+					found = true;
+					foundNum = Double;
+					
+				});
 			} else {
 				System.out.println("Unrecognized input. Please specify 'add' or 'search'");
 			} 
